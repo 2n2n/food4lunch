d5885a1745e568570934b99cb3af5c3a5c0bf952
2n2n-developer <ayolachll@gmail.com>
2015-08-31 23:05:36 +0800

    inital commit

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use App\AuthenticateUser;
+use Validator;
+use Socialite;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+
+class AuthController extends Controller
+{
+    
+    protected $loginPath = "/auth/login";
+    protected $redirectPath = "/dashboard";
+
+    /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
+
+    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'getLogout']);
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|confirmed|min:6',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+
+    public function getLogin() {
+        return view("auth/login");
+    }
+
+    public function postLogin(Request $request) {
+        var_dump($request->get('email'));
+        echo "<pre>";
+        echo print_r(get_class_methods($request),true);
+
+    }
+    public function postRegister(Request $request) {
+        var_dump($request->all());
+    }
+
+    public function redirectToProvider(AuthenticateUser $authenticateUser, Request $request, $provider = null) {
+        return $authenticateUser->execute($request->all(), $this, $provider);
+    }
+
+    public function userHasLoggedIn($user) {
+        \Session::flash('message', 'Welcome, ' . $user->username);
+        return redirect('/dashboard');
+    }
+}
