8b420a15bf922e77cbf9fec94e543bfd735db94d
Anthony Yolach Lloveras <tontonquit@gmail.com>
2015-09-08 14:36:13 +0000

    Form submission now working

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Middleware;
-
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
@@ -11,8 +10,9 @@ class VerifyCsrfToken extends BaseVerifier
      *
      * @var array
      */
+
     protected $except = [
-        'auth/register',
-        'auth/login'
+        // 'auth/register',
+        // 'auth/login'
     ];
 }
