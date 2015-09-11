db0fbf0dd347be51c01920f488cbbf32cd43c5e7
unknown <anthony.ras@SD-021.leekie.com>
2015-09-11 13:20:32 +0800

    still not working

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -86,6 +86,7 @@ class AuthController extends Controller
      */
     public function postLogin(Request $request)
     {
+        dd('yollow');
         $this->validate($request, [
             'email' => 'required|email', 'password' => 'required',
         ]);
@@ -111,7 +112,6 @@ class AuthController extends Controller
         // if ($throttles) {
         //     $this->incrementLoginAttempts($request);
         // }
-        
         return redirect($this->loginPath())
             ->withInput($request->only($this->loginUsername()))
             ->withErrors([
